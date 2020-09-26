d742bfff08a5acbc889b41c7c4d4c1ba29c7768f
Antonio Dinkins <afdinki@gmail.com>
2020-09-26 04:19:50 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -0,0 +1,58 @@
+package com.example.flixster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    String posterPath;
+    String title;
+    String overview;
+//    String posterSize;
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s",posterPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+
+    }
+
+    public static List<Movie> fromJSONArray(JSONArray movieJSONArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for(int i = 0; i < movieJSONArray.length(); i++){
+            movies.add(new Movie(movieJSONArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+
+    /*public static List<String> toStringList(JSONArray movieJSONArray) throws JSONException {
+        List<String> stringList = new ArrayList<>();
+        for(int i = 0; i < movieJSONArray.length; i++){
+            stringList.add(new String(String.valueOf(movieJSONArray.getJSONObject(i))));
+        }
+        return stringList;
+    }
+
+    public String getPosterSize() {
+        return posterSize;
+    }
+
+    public void setPosterSize(String posterSize) {
+        this.posterSize = posterSize;
+    }*/
+}
